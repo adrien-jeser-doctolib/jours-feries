6a0d63897a8dd2f813ba20c58277b06ed38e2235
Adrien Jeser <adrien.jeser@doctolib.com>
2024-01-25 18:39:34 +0100

    Initial commit

---
+++ build.rs
@@ -0,0 +1,39 @@
+#[cfg(feature = "generate")]
+use {
+    std::io::Write,
+    std::{collections::HashMap, fs::File},
+};
+
+#[cfg(feature = "generate")]
+#[tokio::main]
+async fn main() {
+    let json = reqwest::get("https://calendrier.api.gouv.fr/jours-feries/metropole.json")
+        .await
+        .unwrap()
+        .text()
+        .await
+        .unwrap();
+
+    // dbg!(&json.unwrap());
+    let v: HashMap<chrono::NaiveDate, String> = serde_json::from_str(&json).unwrap();
+    let v = v
+        .into_keys()
+        .map(|f| f.to_string())
+        .map(|f| format!("\"{f}\",\n"))
+        .collect::<String>();
+
+    let v = format!(
+        r#"
+pub const JOURS_FERIES: &[&str] = &[
+    {v}
+];
+        "#
+    );
+
+    let output = std::env::var("OUTPUT").unwrap();
+    let mut file = File::create(output).unwrap();
+    file.write_all(v.as_bytes()).unwrap();
+}
+
+#[cfg(not(feature = "generate"))]
+fn main() {}
